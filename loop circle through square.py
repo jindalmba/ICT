caee15647b16f35b7b50f43949a5a3024b7b66a3
jindalmba <143794254+jindalmba@users.noreply.github.com>
2023-09-15 22:51:13 +0530

    Add files via upload

---
+++ loop circle through square.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 10:28:36 2023
+
+@author: ASUS
+"""
+import turtle
+t=turtle.Turtle()
+for i in range(10000):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(22/7)
+    
+    
+    
+    
