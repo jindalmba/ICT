bbd1d99e73126682025bff80be1fd25cbfd9ea1b
jindalmba <143794254+jindalmba@users.noreply.github.com>
2023-09-26 22:33:30 +0530

    Add files via upload

---
+++ triangle loop into circle.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 22:24:16 2023
+
+@author: Tripti
+"""
+import turtle
+t=turtle.Turtle()
+for i in range(10000):
+    t.forward(150)
+    t.left(120)
+    t.forward(150)
+    t.left(120)
+    t.forward(150)
+    t.left(22/7)
