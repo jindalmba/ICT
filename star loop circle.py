4c9f28596fc5740fc1952973112605938085864a
jindalmba <143794254+jindalmba@users.noreply.github.com>
2023-09-26 23:37:11 +0530

    Add files via upload

---
+++ star loop circle.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 23:08:56 2023
+
+@author: Tripti
+"""
+import turtle
+t=turtle.Turtle()
+for i in range(10000):
+    t.forward(180)
+    t.right(135)
+    t.forward(200)
+    t.right(150)
+    t.forward(200)
+    t.right(150)
+    t.forward(200)
+    t.right(150)
+    t.forward(200)
+    t.left(22/7)
