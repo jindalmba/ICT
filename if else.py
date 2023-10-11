a5fab17d1f1910f04619fcb265b80f3ad1694679
jindalmba <143794254+jindalmba@users.noreply.github.com>
2023-10-11 20:53:40 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 10:30:13 2023
+
+@author: ASUS
+"""
+a=1
+if a>0:
+    print('+ve')
+elif a==0:
+    print('zero')
+else:
+    print('-ve')    
---
+++ list range.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 11:31:48 2023
+
+@author: ASUS
+"""
+
